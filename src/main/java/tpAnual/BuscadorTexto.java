4a4ff32a86369de9e0b637218e9931d469085483
PadillaJuan <juanpadilla.jip@gmail.com>
2016-04-15 00:35:14 -0300

    Buscador de texto, commit inicial

--- src/main/java/tpAnual/BuscadorTexto.java
+++ src/main/java/tpAnual/BuscadorTexto.java
@@ -2,19 +2,38 @@ package tpAnual;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BuscadorTexto {
 		
+		//VARIABLES
 		public LevenshteinDistance Buscador = new LevenshteinDistance();
-	
+		
+		//METODOS
+		
+		public String[] SeparaLaBusqueda(String Busqueda){
+			String[] palabras;
+			palabras = Busqueda.split(" ");
+			return palabras;
+		}
+		
+		
 		public List<Poi> BuscameSegunTags (String listaTagsIngresados, List<Poi> listaPois){
-			List<Poi> poisAceptados = new ArrayList<Poi>();
-			
-			
-			
-			
+			String[] palabras = SeparaLaBusqueda(listaTagsIngresados);
+			List<Poi> poisAceptados = listaPois.stream()
+					   .filter(poi->hayInterseccionOInclusion(palabras,poi))
+					   .collect(Collectors.toList());
 			
 			return poisAceptados;
 			
+		}
+
+		public Boolean hayInterseccionOInclusion(String[] palabras, Poi poi) {
+			for (String palabra : palabras){
+				if(poi.tieneTag(palabra)){
+					return true;
+				}
+			}
+			return false;
 		}
 }
