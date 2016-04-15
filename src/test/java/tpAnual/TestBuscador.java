026013587ff2929b44d2678c26de3350c0cf61ef
PadillaJuan <juanpadilla.jip@gmail.com>
2016-04-15 01:09:16 -0300

    Test del buscador minimo

---
+++ src/test/java/tpAnual/TestBuscador.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.*;
+
+import tpAnual.*;
+
+public class TestBuscador {
+	private Set<String> tags = new HashSet<String>();
+	private Colectivo tipo = new Colectivo();
+	private Poi poi = new Poi(tipo, "107", tags);
+	List<Poi> listaPois = new ArrayList<Poi>();
+	Mapa mapa = new Mapa();
+	@Before
+	public void init(){
+		poi.agregarTag("107");
+		poi.agregarTag("colectivo");
+		mapa.agregarPoi(poi);
+	}
+	@Test
+	public void busca107SegunLibre(){
+		int i = mapa.buscarPoi("107").size();
+		Assert.assertEquals(1,i);
+	}
+}
