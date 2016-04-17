80419f62b046c34ff3ba563536ef409c51701996
CampaUTN <rodrigocampassi@hotmail.com>
2016-04-17 13:52:13 -0300

    agregados test cercania

--- src/test/java/tpAnual/TestCercania.java
+++ src/test/java/tpAnual/TestCercania.java
@@ -13,12 +13,18 @@ public class TestCercania {
 	private Point puntoComunaC = new Point(50,0);
 	
 	private Set<String> tags = new HashSet<String>();
-	private Cgp cgp = new Cgp(puntoComunaA,puntoComunaB,puntoComunaC);
 	private Point ubicacionPoi = new Point(20, 10);
 	
-	private Poi poi = new Poi(cgp, ubicacionPoi, "107", tags);
+	private Cgp cgp = new Cgp(puntoComunaA,puntoComunaB,puntoComunaC);
+	private Banco banco = new Banco();
+	private Colectivo colectivo = new Colectivo();
 	
-	private Point puntoDeBusqueda = new Point(21,11);
+	private Poi poiColectivo = new Poi(colectivo,ubicacionPoi,"60",tags);
+	private Poi poiCgp = new Poi(cgp, ubicacionPoi, "Asistencia social", tags);
+	private Poi poiBanco = new Poi(banco, ubicacionPoi, "banco galicia", tags);
+	
+	private Point puntoDeBusqueda = new Point(20.003,10.003);
+	private Point puntoDeBusquedaColect = new Point (20.0006,10.0003);
 	
 	@Before
 	public void init() {
@@ -27,7 +33,17 @@ public class TestCercania {
 
 	@Test
 	public void cgpEstaCercaPorComuna() {
-		Assert.assertTrue(poi.estaCerca(puntoDeBusqueda));
-		
+		Assert.assertTrue(poiCgp.estaCerca(puntoDeBusqueda));
 	}
+	
+	@Test
+	public void bancoEstaCercaPorMenosDe5Cuadras() {
+		Assert.assertTrue(poiBanco.estaCerca(puntoDeBusqueda));
+	}
+	
+	@Test
+	public void paradaDeColectivoEstaCercaPorMenosDe1Cuadra(){
+		Assert.assertTrue(poiColectivo.estaCerca(puntoDeBusquedaColect));
+	}
+	
 }
