bdea7c9340542fb4fb12368a587e6c4c41b6887b
emitolaba95 <emi.tolaba95@gmail.com>
2016-04-18 00:40:53 -0300

    Subo implementacion de clase Horario

--- src/main/java/tpAnual/Horario.java
+++ src/main/java/tpAnual/Horario.java
@@ -1,7 +1,35 @@
 package tpAnual;
 
-public class Horario {
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class Horario  {
+	private DayOfWeek diaDesde;
+	private DayOfWeek diaHasta;
+	private LocalTime desde;
+	private LocalTime hasta;
 	
-	//ke azemo?
 	
+	public boolean estaEnFranjaHoraria(DayOfWeek dia,LocalTime hora){
+		
+		 boolean cumpleDia = (dia.getValue() >= diaDesde.getValue()) && (dia.getValue() <= diaHasta.getValue()); //esta entre el diaDesde y el diaHasta
+		 
+		 boolean estaEnHorario = (hora.isAfter(this.desde) && hora.isBefore(this.hasta));  //estaDentro del horario especificado
+		 return cumpleDia && estaEnHorario;
+	}
+
+	Horario(DayOfWeek inicio,DayOfWeek fin, String desde, String hasta){ //se usa si es un rango de dias
+		this.diaHasta = fin;
+		this.diaDesde = inicio;
+		this.desde= LocalTime.parse(desde);
+		this.hasta= LocalTime.parse(hasta);
+	}
+	
+	
+	Horario(DayOfWeek inicio, String desde, String hasta){   //se usa por si es un unico dia
+		this.diaDesde = inicio;
+		this.diaHasta = inicio;
+		this.desde= LocalTime.parse(desde);
+		this.hasta= LocalTime.parse(hasta);
+	}
 }
