b03842f0b1d4884d4e4814fcc2e72cbf9535aa1d
emitolaba95 <emi.tolaba95@gmail.com>
2016-04-18 02:00:21 -0300

    Implementacion de horario para Banco

--- src/main/java/tpAnual/Banco.java
+++ src/main/java/tpAnual/Banco.java
@@ -1,11 +1,14 @@
 package tpAnual;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.List;
 import org.uqbar.geodds.Point;
 
 public class Banco extends TipoPoi{
 
 	private List<String> servicios;
+	private Horario horarioAtencion;
 	
 	@Override
 	public boolean brinda(String servicio)
@@ -14,19 +17,30 @@ public class Banco extends TipoPoi{
 	}
 	
 	@Override
-	public boolean estaDisponible() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean estaDisponible(DayOfWeek dia,String hora) {
+		
+		return estaEnFranjaHoraria(dia, LocalTime.parse(hora));
 	}
 	
-	public boolean estaDisponible(String servicio)
+	@Override
+	public boolean estaDisponible(String servicio,DayOfWeek dia,String hora)
 	{
-		return this.brinda(servicio) && this.estaDisponible();
+		return this.brinda(servicio) && this.estaDisponible(dia,hora);
 	}
 
 	public void agregarServicio(String servicio){
 		servicios.add(servicio);
 	}	
+	
+	
+	public Boolean estaEnFranjaHoraria(DayOfWeek dia,LocalTime hora){
+		return horarioAtencion.estaEnFranjaHoraria(dia, hora);
+	}
+			
+	public Banco()
+	{
+		this.horarioAtencion =new Horario(DayOfWeek.MONDAY,DayOfWeek.FRIDAY,"10:00", "17:00");
+	}
 	
 	//El metodo de estaCerca de banco corresponde al default de 5 cuadras definido en TipoPoi
 }
