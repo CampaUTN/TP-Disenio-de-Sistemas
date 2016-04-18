0577b58cc01d38105cf5a312f12def5a80205fef
CampaUTN <rodrigocampassi@hotmail.com>
2016-04-18 16:09:43 -0300

    Fix test cercania que la manquie mal

--- src/test/java/tpAnual/TestCercania.java
+++ src/test/java/tpAnual/TestCercania.java
@@ -1,5 +1,6 @@
 package tpAnual;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,7 +13,7 @@ public class TestCercania {
 	private Point puntoComunaA = new Point(0,0);
 	private Point puntoComunaB = new Point(20,60);
 	private Point puntoComunaC = new Point(50,0);
-	List<Point> puntosComuna;
+	private List<Point> puntosComuna =  new ArrayList<Point>();
 	
 	private Set<String> tags = new HashSet<String>();
 	private Point ubicacionPoi = new Point(20, 10);
@@ -28,13 +29,16 @@ public class TestCercania {
 	private Point puntoDeBusqueda = new Point(20.003,10.003);
 	private Point puntoDeBusquedaColect = new Point (20.0006,10.0003);
 	
-	@Before
-	public void init() {
+	public TestCercania(){
 		puntosComuna.add(puntoComunaA);
 		puntosComuna.add(puntoComunaB);
 		puntosComuna.add(puntoComunaC);
 		cgp = new Cgp(puntosComuna);
 		poiCgp = new Poi(cgp, ubicacionPoi, "Asistencia social", tags);
+	}
+	
+	@Before
+	public void init() {
 	}
 
 	@Test
