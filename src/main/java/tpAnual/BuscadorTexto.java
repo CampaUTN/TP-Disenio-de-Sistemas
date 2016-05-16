a3d899f6d0c4c07b53cc03dc6edfe0f08e3254aa
campautn <rodrigocampassi@hotmail.com>
2016-05-16 20:18:33 -0300

    dejo todo delegado para la busqueda

--- src/main/java/tpAnual/BuscadorTexto.java
+++ src/main/java/tpAnual/BuscadorTexto.java
@@ -2,32 +2,53 @@ package tpAnual;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BuscadorTexto {
 	
+	BancoAdapter bancoAdap;
+	CGPAdapter cgpAdap;
+	
 	private List<String> separaLaBusqueda(String Busqueda) {
 		return Arrays.asList(Busqueda.split(" "));
 	}
-
-//	public List<Poi> buscameSegunTags(String listaTagsIngresados, List<Poi> listaPois) {
-//		List<String> palabras = separaLaBusqueda(listaTagsIngresados);
-//		return listaPois.stream()
-//				.filter(poi -> hayInterseccionOInclusion(palabras, poi))
-//				.collect(Collectors.toList());
-//	}
-
-//	private boolean hayInterseccionOInclusion(List<String> palabras, Poi poi) {
-//		return palabras.stream()
-//				.anyMatch(palabra -> poi.tieneTag(palabra));
-//	}
 	
 	public List<Poi> buscarSegunTexto(String palabrasIngresadas, List<Poi> listaPois){
 		List<String> palabras = separaLaBusqueda(palabrasIngresadas);
+		List<Poi> resultado = new ArrayList<Poi>();
+		
+		resultado.addAll(buscarEnPoisLocales(palabras, listaPois));
+		resultado.addAll(this.buscarSistemaCgpExterno(palabras));
+		resultado.addAll(this.buscarSistemaBancoExterno(palabras));
+		return resultado;
+	}
+	
+	public List<Poi> buscarSistemaCgpExterno(List<String> palabras){
+		
+		return null; //Provisorio para que compile
+	}
+	
+	public List<Poi> buscarSistemaBancoExterno(List<String> palabras){
+		
+		return null; //Provisorio para que compile
+	}
+	
+	public List<Poi> buscarEnPoisLocales(List<String> palabras, List<Poi> listaPois){
 		return listaPois.stream()
 				.filter(poi-> poi.cumpleCondicionBusqueda(palabras))
 				.collect(Collectors.toList());
 	}
 	
+	
+	//Setters
+	
+		public void setBancoAdapter(BancoAdapter bancoAd){
+			bancoAdap = bancoAd;
+		}
+		public void setCgpAdapter(CGPAdapter cgpAd){
+			cgpAdap = cgpAd;
+		}
+
 	
 }
