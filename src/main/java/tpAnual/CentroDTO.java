69e7cb8eb027aafcce4963e40e3d3938743b9dcb
LucasEsposito <espositolucas95@gmail.com>
2016-05-16 01:00:26 -0300

    agrego adapters

---
+++ src/main/java/tpAnual/CentroDTO.java
@@ -0,0 +1,10 @@
+package tpAnual;
+
+public class CentroDTO {
+	int numeroComuna;
+	String zonas;
+	String nombreDirector;
+	String domicilio;
+	String telefono;
+	// TODO hacer la lista de serviciosDTO
+}
