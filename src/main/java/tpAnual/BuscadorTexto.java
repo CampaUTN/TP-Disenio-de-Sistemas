edfa96857c5010fa7666ce064870b146022a7f30
campautn <rodrigocampassi@hotmail.com>
2016-05-18 14:06:36 -0300

    volvemo

--- src/main/java/tpAnual/BuscadorTexto.java
+++ src/main/java/tpAnual/BuscadorTexto.java
@@ -14,19 +14,30 @@ public class BuscadorTexto {
 		return Arrays.asList(Busqueda.split(" "));
 	}
 	
+	public List<Poi> buscarSegunTexto(String palabrasIngresadas, List<Poi> listaPois){
+		List<String> palabras = separaLaBusqueda(palabrasIngresadas);
+		List<Poi> resultado = new ArrayList<Poi>();
+		
+		resultado.addAll(buscarEnPoisLocales(palabras, listaPois));
+		resultado.addAll(this.buscarSistemaCgpExterno(palabras));
+		resultado.addAll(this.buscarSistemaBancoExterno(palabras));
+		return resultado;
+	}
+	
 	public List<Poi> buscarSistemaCgpExterno(List<String> palabras){
 //		cgpAdap.consultar(palabras);
 		// Aca en realidad no consultamos sino que solo recibe??
 		return null; //Provisorio para que compile
 	}
 	
-	public List<Poi> buscarSistemaBancoExterno(String nombre, String servicio){
-		return bancoAdap.consultar(nombre, servicio);
+	public List<Poi> buscarSistemaBancoExterno(List<String> palabras){
+//		bancoAdap.consultar(palabras);
+		return null; //Provisorio para que compile
 	}
 	
-	public List<Poi> busquedaTextoLibre(String tags, List<Poi> listaPois){
+	public List<Poi> buscarEnPoisLocales(List<String> palabras, List<Poi> listaPois){
 		return listaPois.stream()
-				.filter(poi-> poi.cumpleCondicionBusqueda(this.separaLaBusqueda(tags)))
+				.filter(poi-> poi.cumpleCondicionBusqueda(palabras))
 				.collect(Collectors.toList());
 	}
 	
