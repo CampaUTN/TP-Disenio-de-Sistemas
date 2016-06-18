d7f94c9271a70275948a1c8d2536f104bb4f1a84
LucasEsposito <espositolucas95@gmail.com>
2016-06-18 17:01:53 -0300

    fix testEmailSender

--- src/test/java/tpAnual/TestEmailSenderBusqueda.java
+++ src/test/java/tpAnual/TestEmailSenderBusqueda.java
@@ -15,13 +15,14 @@ public class TestEmailSenderBusqueda extends TestBuscador{
 		super.init();
 		sender = Mockito.mock(EmailSender.class);
 		mapa.getBuscador().setSender(sender);
+		terminal = new Terminal(1);
 	}
 	
 	@Test
 	public void testearMailPorDemora(){
 		// Testeo con 0 como limite porque es imposible que tarde menos que 0.
 		mapa.getBuscador().setLimite((long) 0);
-		mapa.buscar("colectivo",null);
+		mapa.buscar("colectivo",terminal);
 		Mockito.verify(sender).enviarMensajePorDemora((long) 0);
 	}
 	
@@ -29,7 +30,7 @@ public class TestEmailSenderBusqueda extends TestBuscador{
 	public void testMail(){
 		// Por default no deberia tardar, porque los test usan mocks asi que no hay mas retardo que el de
 		// la maquina donde se corran, y el limite por defecto esta seteado en un valor altisimo.
-		mapa.buscar("colectivo",null);
+		mapa.buscar("colectivo",terminal);
 		Mockito.verifyNoMoreInteractions(sender);
 	}
 }
