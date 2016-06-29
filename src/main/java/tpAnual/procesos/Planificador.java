a25a138269db482ad036af547e26ccf72e4333a2
LucasEsposito <espositolucas95@gmail.com>
2016-06-29 15:15:42 -0300

    mini cambio

--- src/main/java/tpAnual/procesos/Planificador.java
+++ src/main/java/tpAnual/procesos/Planificador.java
@@ -6,12 +6,9 @@ import java.time.LocalTime;
 import tpAnual.procesos.operaciones.Proceso;
 
 public class Planificador implements IPlanificador{
-
-	private static Planificador instance = null;
-	private Lanzador lanzador = Lanzador.getInstance();
 	
 	public void programarProceso(Proceso proceso,LocalDate fecha,LocalTime hora){
-		lanzador.agregarProceso(proceso);
+		Lanzador.getInstance().agregarProceso(proceso);
 	}
 	
 	public void programarProcesoRutinario(Proceso proceso, LocalTime Fecha){
