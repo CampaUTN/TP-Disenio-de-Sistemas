65ed47d76053da04a7a85cba3237e4399424c5a6
datto96 <lucasdattoli96@gmail.com>
2016-06-29 15:15:15 -0300

    agrego intento de lectura de url

--- src/main/java/tpAnual/externo/adapters/BajaPoiAdapter.java
+++ src/main/java/tpAnual/externo/adapters/BajaPoiAdapter.java
@@ -1,11 +1,12 @@
 package tpAnual.externo.adapters;
 
 import java.io.File;
+
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
-
+import tpAnual.procesos.operaciones.Proceso;
 import java.util.stream.Collectors;
 
 import tpAnual.Mapa;
@@ -17,9 +18,66 @@ import org.apache.commons.io.FileUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.sun.jersey.api.client.Client;
+
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import javax.ws.rs.core.MediaType;
 
 
 public class BajaPoiAdapter {
+	
+	    private Client client;
+	    private static final String API_GOOGLE = "http://demo3537367.mockable.io/trash";
+	    private static final String RESOURCE = "pois";
+
+	    //Inicializacion del cliente.
+	    public BajaPoiAdapter() {
+	        this.client = Client.create();
+	    }
+	    
+	    
+	  //Prueba de concepto de un parametro y los mensajes por separado para identificar los tipos de datos.
+	    public ClientResponse getBookByFilter(String filter, String value){
+	        WebResource recurso = this.client.resource(API_GOOGLE).path(RESOURCE);
+	        WebResource recursoConParametros = recurso.queryParam("q",filter + ":" + value);
+	        WebResource.Builder builder = recursoConParametros.accept(MediaType.APPLICATION_JSON);
+	        ClientResponse response = builder.get(ClientResponse.class);
+	        return response;
+	    }
+	    
+//	    public ClientResponse getBookByFilter(String filter, String value, String fields){
+//	        ClientResponse response = this.client.resource(API_GOOGLE).path(RESOURCE)
+//	                .queryParam("q",value).queryParam("fields",fields)
+//	                .accept(MediaType.APPLICATION_JSON)
+//	                .get(ClientResponse.class);
+//	        return response;
+//	    }
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
 	
     private MockBajaPoi poiAEliminar = new MockBajaPoi();
     
@@ -56,4 +114,6 @@ public class BajaPoiAdapter {
 		    Mapa.getInstance().baja(Mapa.getInstance().pois.stream()
 		    	    .filter(poi -> poi.getId() == id).collect(Collectors.toList()).get(0));
 		}
+	 
+	 
 }
