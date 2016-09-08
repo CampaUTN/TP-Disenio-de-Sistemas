ef68d1f5545ad3dc9b16c5dee2081d4eee43ee91
LucasEsposito <espositolucas95@gmail.com>
2016-09-07 21:05:28 -0300

    agrego context test bd

---
+++ src/test/java/tpAnual/bd/ContextTest.java
@@ -0,0 +1,22 @@
+package tpAnual.bd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
+
+/* Tests para ver que no hay errores con la BD, no usar para otra cosa */
+public class ContextTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
+
+	@Test
+	public void contextUp() {
+		assertNotNull(entityManager());
+	}
+
+	@Test
+	public void contextUpWithTransaction() throws Exception {
+		withTransaction(() -> {});
+	}
+
+}
