7b1e4a2af872eadb2bd8c8d8937217bc3d96659e
CampaUTN <rodrigocampassi@hotmail.com>
2016-09-13 20:02:49 -0300

    agrego activacionPorComuna que borre sin querer

---
+++ src/main/java/tpAnual/batch/procesos/ActivacionPorComuna.java
@@ -0,0 +1,33 @@
+package tpAnual.batch.procesos;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import tpAnual.Mapa;
+import tpAnual.Terminal;
+
+public class ActivacionPorComuna extends Proceso{
+	
+	Set<Terminal> terminales = new HashSet<Terminal>();
+	Integer comuna;
+	Set<AccionTerminal> acciones = new HashSet<AccionTerminal>();
+ 	
+ 	public ActivacionPorComuna(Integer numeroComuna,Set<AccionTerminal> acciones){
+ 		this.acciones = acciones;
+ 		this.comuna = numeroComuna;
+ 	}
+ 	
+ 	public void ejecutar(){
+ 		terminales = this.seleccionarTerminales();
+ 		acciones.forEach(accion -> accion.realizarAccion(terminales));
+ 	}
+ 	
+ 	private Set<Terminal> seleccionarTerminales(){
+ 		Set<Terminal> terminales = new HashSet<Terminal>();
+ 		terminales = Mapa.getInstance().terminales();
+ 		
+ 		return terminales.stream()
+ 				.filter(terminal -> terminal.getNumeroComuna().equals(comuna)).collect(Collectors.toSet());
+ 	}
+ }
