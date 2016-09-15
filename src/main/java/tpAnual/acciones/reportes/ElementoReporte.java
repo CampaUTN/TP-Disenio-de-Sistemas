a42cc12561758f492177f24087285bac59795351
datto96 <lucasdattoli96@gmail.com>
2016-09-15 00:35:59 -0300

    The negrada begins pt 2 (ElementoReporte)

--- src/main/java/tpAnual/acciones/reportes/ElementoReporte.java
+++ src/main/java/tpAnual/acciones/reportes/ElementoReporte.java
@@ -1,17 +1,41 @@
 package tpAnual.acciones.reportes;
 
 import java.time.LocalDate;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import tpAnual.Terminal;
+import tpAnual.bd.LocalDateConverter;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Entity
 public class ElementoReporte {
 	
+	@Id @GeneratedValue @Column (name="cod_elemento")
+	private long numeroElemento;
+	@Convert(converter = LocalDateConverter.class)
 	private LocalDate fecha;
 	private Integer cantidadBusquedas=0;
 	private Terminal terminal;
 	private Integer cantidadPoisEncontrados=0;
+	@ElementCollection @Cascade({CascadeType.ALL})
 	private List<Integer> busquedasParciales = new ArrayList<Integer>();
 	
 	public boolean esDeLaFecha(LocalDate fecha){
@@ -82,5 +106,25 @@ public class ElementoReporte {
 
 	public Integer getCantidadPoisEncontrados() {
 		return cantidadPoisEncontrados;
+	}
+
+	public long getNumeroElemento() {
+		return numeroElemento;
+	}
+
+	public void setNumeroElemento(long numeroElemento) {
+		this.numeroElemento = numeroElemento;
+	}
+
+	public void setCantidadBusquedas(Integer cantidadBusquedas) {
+		this.cantidadBusquedas = cantidadBusquedas;
+	}
+
+	public void setCantidadPoisEncontrados(Integer cantidadPoisEncontrados) {
+		this.cantidadPoisEncontrados = cantidadPoisEncontrados;
+	}
+
+	public void setBusquedasParciales(List<Integer> busquedasParciales) {
+		this.busquedasParciales = busquedasParciales;
 	}
 }
