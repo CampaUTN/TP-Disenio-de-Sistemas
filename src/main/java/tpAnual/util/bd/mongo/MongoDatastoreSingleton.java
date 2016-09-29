391e3a52689934bb95d0f4624e2f83ca4efafc95
LucasEsposito <espositolucas95@gmail.com>
2016-09-28 22:23:31 -0300

    [datastore] saco excepciones y codigo comentado

--- src/main/java/tpAnual/util/bd/mongo/MongoDatastoreSingleton.java
+++ src/main/java/tpAnual/util/bd/mongo/MongoDatastoreSingleton.java
@@ -1,8 +1,5 @@
 package tpAnual.util.bd.mongo;
 
-import java.net.UnknownHostException;
-import java.util.ResourceBundle;
-
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
@@ -14,15 +11,10 @@ public class MongoDatastoreSingleton {
 	private static Morphia morphiaSingleton = null;
 	private static Datastore datastoreSingleton = null;
 	
-	private static MongoClient getMongoClient(String nombreBD) throws UnknownHostException {
+	private static MongoClient getMongoClient(String nombreBD) {
 		if (mongoSingleton == null) {
 			synchronized (MongoDatastoreSingleton.class) {
 				if (mongoSingleton == null) {
-//					ResourceBundle bundle = ResourceBundle.getBundle(nombreBD);
-//					String host = bundle.getString("host");
-//					int port = Integer.parseInt(bundle.getString("port"));
-
-//					mongoSingleton = new MongoClient(host, port);
 					mongoSingleton = new MongoClient("localhost", 27017);
 				}
 			}
@@ -37,7 +29,7 @@ public class MongoDatastoreSingleton {
 		return morphiaSingleton;
 	}
 	
-	public static Datastore getDatastore(String nombreBD) throws UnknownHostException{
+	public static Datastore getDatastore(String nombreBD){
 		if(datastoreSingleton==null){
 			datastoreSingleton = getMorphia().createDatastore(getMongoClient(nombreBD), nombreBD);
 		}
