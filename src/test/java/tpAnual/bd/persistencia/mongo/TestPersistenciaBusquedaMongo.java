b5e94cb5cbad477ae8e74290e7c6c55edf394e4b
emitolaba95 <emi.tolaba95@gmail.com>
2016-09-29 01:18:17 -0300

    Separo tests de PersistenciaBusqueda

--- src/test/java/tpAnual/bd/persistencia/mongo/TestPersistenciaBusquedaMongo.java
+++ src/test/java/tpAnual/bd/persistencia/mongo/TestPersistenciaBusquedaMongo.java
@@ -32,8 +32,6 @@ import tpAnual.util.wrapper.PointWrapper;
 
 public class TestPersistenciaBusquedaMongo {
 	
-//	private static Datastore datastore = morphia.createDatastore(mongo, DB_Name);
-
 	private static Datastore datastore;
 	
 	private Set<String> tags1 = new HashSet<String>();
@@ -44,15 +42,12 @@ public class TestPersistenciaBusquedaMongo {
 	private Poi poi2 = new EstacionDeColectivo(ubicacion, "106", tags2,0,"");
 
 	private PoiDTO poiDto1;
-	private PoiDTO poiDto2;
 	
 	private Terminal terminal = new Terminal();
 	
 	private BuscadorLocal local = new BuscadorLocal();
 	private BuscadorTexto buscador = new BuscadorTexto();		
-	
-//	Query<PoiDTO> query = datastore.createQuery(PoiDTO.class);
-	
+		
 	@BeforeClass
 	public static void initClass() throws UnknownHostException{
 		datastore = MongoDatastoreSingleton.getDatastore("busquedas");
@@ -73,7 +68,6 @@ public class TestPersistenciaBusquedaMongo {
 		poi2.agregarTag("chocador");
 		
 		poiDto1 = PoiDTO.nuevoDesdePoi(poi1);
-		poiDto2 = PoiDTO.nuevoDesdePoi(poi2);
 				
 		terminal.desactivarMails();
 		terminal.desactivarRegistros();
@@ -90,63 +84,6 @@ public class TestPersistenciaBusquedaMongo {
 		Reseter.resetDatastore(datastore);
 	}
 	
-	// ---- Banco externo -----
-	
-	@Test
-	public void sePersisteElBancoExterno(){
-		BancoDTO bancoExterno = new BancoDTO();
-		bancoExterno.setBanco("galicia");
-		String[] servicios = {"pago cheques", "consultas"};
-		bancoExterno.setServicios(servicios);
-		
-		datastore.save(bancoExterno);
-		
-		Assert.assertFalse(datastore.createQuery(BancoDTO.class).asList().isEmpty());
-	}
-	
-	@Test
-	public void sePersistenLosServiciosDelBancoExterno(){
-		BancoDTO bancoExterno = new BancoDTO();
-		bancoExterno.setBanco("galicia");
-		String[] servicios = {"pago cheques", "consultas"};
-		bancoExterno.setServicios(servicios);
-		
-		datastore.save(bancoExterno);
-		
-		Assert.assertEquals(2, datastore.createQuery(BancoDTO.class).asList().get(0).getServicios().length,0);
-	}
-	// -------
-	
-	// ---- Centro DTO -----
-	
-	@Test
-	public void sePersisteCentroDTO(){
-		CentroDTO centro = new CentroDTO();
-		List<ServicioDTO> servicios = new ArrayList<ServicioDTO>();
-		ServicioDTO unServicio = new ServicioDTO("consultas", 3, 10, 00, 18, 00);
-		servicios.add(unServicio);
-		centro.setServicios(servicios);
-		
-		datastore.save(centro);
-
-		Assert.assertFalse(datastore.createQuery(CentroDTO.class).asList().isEmpty());
-	}
-	
-	@Test
-	public void sePersistenLosServiciosDeCentroDTO(){
-		CentroDTO centro = new CentroDTO();
-		List<ServicioDTO> servicios = new ArrayList<ServicioDTO>();
-		ServicioDTO unServicio = new ServicioDTO("consultas", 3, 10, 00, 18, 00);
-		servicios.add(unServicio);
-		centro.setServicios(servicios);
-		
-		datastore.save(centro);
-
-		Assert.assertEquals(1, datastore.createQuery(CentroDTO.class).asList().get(0).getServicios().size(),0);
-	}
-	
-	//-------
-	
 	@Test
 	public void comprueboQueNoHayNadaPersistido() throws UnknownHostException{		
 		Assert.assertTrue(datastore.createQuery(Busqueda.class).asList().isEmpty());
@@ -162,8 +99,7 @@ public class TestPersistenciaBusquedaMongo {
 		datastore.save(busqueda);		
 		
 		Assert.assertFalse(datastore.createQuery(Busqueda.class).asList().isEmpty());
-	}
-	
+	}	
 	
 	@Test 
 	public void seRealizaUnaBusquedaYLuegoSePersiste(){
@@ -181,27 +117,6 @@ public class TestPersistenciaBusquedaMongo {
 		long cantidadPois = datastore.find(Busqueda.class).get().getResultado().size();
 			
 		Assert.assertEquals(pois.size(),cantidadPois);
-	}
-	
-	//Saque las queries de aca http://mongodb.github.io/morphia/1.0/guides/querying/
-	
-	@Test
-	public void testDePrueba01(){
-		datastore.save(poiDto1);
-		PoiDTO poiEncontrado = datastore.createQuery(PoiDTO.class)
-								.filter("nombre","107").asList().get(0);
-		
-		Assert.assertEquals(2, poiEncontrado.getTagsPoi().size(),0);
-	}
-	
-	
-	@Test
-	public void testDePrueba02(){
-		datastore.save(poiDto2);
-		PoiDTO poiEncontra = datastore.createQuery(PoiDTO.class)
-								.filter("nombre","106").asList().get(0);
-		
-		Assert.assertEquals(1, poiEncontra.getTagsPoi().size(),0);
 	}
 	
 }
