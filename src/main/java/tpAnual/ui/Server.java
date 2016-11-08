b6f803d0c4ec3fdb3bd07e2f57853efad4caa589
LucasEsposito <espositolucas95@gmail.com>
2016-11-08 20:37:19 -0300

    agrego css de internet :p

---
+++ src/main/java/tpAnual/ui/Server.java
@@ -0,0 +1,38 @@
+package tpAnual.ui;
+
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+public class Server {
+	public static void main(String[] args) {
+		
+		
+		configurarSpark();
+		
+		 //Cuando recibe una request a esa ruta (/), ejecuta la lambda.
+		Spark.get("/", (request, response) -> { return estilizar("<html><body><h1>hola mundo!</h1></body></html>");} );
+		 
+		//localhost:4567/nombre?=Juan
+		/* Spark.get("/",(request, response) -> {
+			String nombreVistante = request.queryParams("nombre");
+			return "<html><body><h1>hola " + nombreVistante + "!</h1></body></html>";
+			} ); */
+		
+		//localhost:4567/saludador/juan
+//		Spark.get("/saludador/:nombre", (request, response) -> {
+//			String nombreVistante = request.params("nombre");
+//			return "<html><body><h1>hola " + nombreVistante + "!</h1></body></html>";
+//			} );
+	}
+	
+	
+	public static void configurarSpark(){
+		Spark.port(4568);
+		Spark.staticFileLocation("/ui");
+	}
+	
+	public static String estilizar(String contenido){
+		return contenido;
+		//return "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" media=\"screen\" />"+contenido;
+	}
+}
