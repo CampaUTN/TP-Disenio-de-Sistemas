59309ca2c5f40eec4c72f617b1615476af8c2da3
LucasEsposito <espositolucas95@gmail.com>
2016-11-10 01:50:08 -0300

    limpio codigo spark

--- src/main/java/tpAnual/ui/Server.java
+++ src/main/java/tpAnual/ui/Server.java
@@ -7,25 +7,8 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class Server {
 	public static void main(String[] args) {
-		
-		
 		configurarSpark();
 		Router.configure();
-		
-		 //Cuando recibe una request a esa ruta (/), ejecuta la lambda.
-		//Spark.get("/", (request, response) -> { return estilizar("<html><body><h1>hola mundo!</h1></body></html>");} );
-		 
-		//localhost:4567/nombre?=Juan
-		/* Spark.get("/",(request, response) -> {
-			String nombreVistante = request.queryParams("nombre");
-			return "<html><body><h1>hola " + nombreVistante + "!</h1></body></html>";
-			} ); */
-		
-		//localhost:4567/saludador/juan
-//		Spark.get("/saludador/:nombre", (request, response) -> {
-//			String nombreVistante = request.params("nombre");
-//			return "<html><body><h1>hola " + nombreVistante + "!</h1></body></html>";
-//			} );
 	}
 	
 	
@@ -36,7 +19,6 @@ public class Server {
 	
 	public static String estilizar(String contenido){
 		return contenido;
-		//return "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" media=\"screen\" />"+contenido;
 	}
 	
 	public static String paginaPrincipal(Request req, Response res){
