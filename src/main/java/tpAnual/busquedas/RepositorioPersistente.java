abb18d371a1818d7785612b0f1a8d5abf97f45cd
LUCAS DATTOLI <lucasdattoli96@gmail.com>
2016-11-24 03:07:50 -0300

    sigo intentando con el historial

--- src/main/java/tpAnual/busquedas/RepositorioPersistente.java
+++ src/main/java/tpAnual/busquedas/RepositorioPersistente.java
@@ -1,12 +1,17 @@
 package tpAnual.busquedas;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-
-
+import tpAnual.POIs.Banco;
+import tpAnual.POIs.Negocio;
 import tpAnual.POIs.Poi;
 import tpAnual.externo.sistemasExternos.BancoDTO;
 import tpAnual.util.bd.mongo.MongoDatastoreSingleton;
+import tpAnual.util.wrapper.PointWrapper;
+
 import org.mongodb.morphia.Datastore;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 
@@ -22,6 +27,30 @@ public class RepositorioPersistente extends RepositorioBusqueda implements WithG
 	}
 	
 	public List<Busqueda> listar(String fechaDesde, String fechaHasta) {
+		
+		List<Poi> pois = new ArrayList<>();
+		
+		Poi poi1 = (Poi)new Negocio(new PointWrapper(0, 0),"Negocio",null,"compras",5);
+		Poi poi2 = (Poi) new Banco(new PointWrapper(2, 2), "Banco Santander" , null);
+		
+		poi1.setCalle("Strangford");
+		poi1.setDireccion(1857);
+		
+		poi2.setCalle("Avenida Rivadavia");
+		poi2.setDireccion(458);
+		
+		pois.add(poi1);
+		pois.add(poi2);
+		
+		Busqueda b1 = new Busqueda("hola", pois);
+		b1.setFecha(Date.valueOf(LocalDate.now()));
+		
+		//BORRAR ESTO DE PRUEBA
+		List<Busqueda> busqueda = new ArrayList<>();
+		busqueda.add(b1);
+		
+		datastore.save(busqueda);
+		
 		List<Busqueda> busquedas = datastore.createQuery(Busqueda.class).asList();
 		return busquedas;
 	}
--- src/main/java/tpAnual/ui/controllers/BusquedasController.java
+++ src/main/java/tpAnual/ui/controllers/BusquedasController.java
@@ -26,40 +26,33 @@ public class BusquedasController {
 
 
 	
-	public static ModelAndView listar(Request req, Response res)
+	public static ModelAndView listarHardcodeado(Request req, Response res)
 	{
 		Map<String, List<Busqueda>> model = new HashMap<>();
-		String fechaDesde = req.params("fechaDesde");
-		String fechaHasta = req.params("fechaHasta");
-		//String cantidadPois = req.params("cantidadPois");
-		//String terminal = req.params("terminal");
-		
-		List<Busqueda> busqueda = (new RepositorioPersistente()).listar(fechaDesde, fechaHasta);
-		busqueda.addAll(RepositorioNoPersistente.getInstance().listar(fechaDesde,fechaHasta));
 		
 		
 		
 		
-//		List<Poi> pois = new ArrayList<>();
-//		
-//		Poi poi1 = (Poi)new Negocio(new PointWrapper(0, 0),"Negocio",null,"compras",5);
-//		Poi poi2 = (Poi) new Banco(new PointWrapper(2, 2), "Banco Santander" , null);
-//		
-//		poi1.setCalle("Strangford");
-//		poi1.setDireccion(1857);
-//		
-//		poi2.setCalle("Avenida Rivadavia");
-//		poi2.setDireccion(458);
-//		
-//		pois.add(poi1);
-//		pois.add(poi2);
-//		
-//		Busqueda b1 = new Busqueda("hola", pois);
-//		b1.setFecha(Date.valueOf(LocalDate.now()));
-//		
-//		//BORRAR ESTO DE PRUEBA
-//		List<Busqueda> busqueda = new ArrayList<>();
-//		busqueda.add(b1);
+		List<Poi> pois = new ArrayList<>();
+		
+		Poi poi1 = (Poi)new Negocio(new PointWrapper(0, 0),"Negocio",null,"compras",5);
+		Poi poi2 = (Poi) new Banco(new PointWrapper(2, 2), "Banco Santander" , null);
+		
+		poi1.setCalle("Strangford");
+		poi1.setDireccion(1857);
+		
+		poi2.setCalle("Avenida Rivadavia");
+		poi2.setDireccion(458);
+		
+		pois.add(poi1);
+		pois.add(poi2);
+		
+		Busqueda b1 = new Busqueda("hola", pois);
+		b1.setFecha(Date.valueOf(LocalDate.now()));
+		
+		//BORRAR ESTO DE PRUEBA
+		List<Busqueda> busqueda = new ArrayList<>();
+		busqueda.add(b1);
 		
 		
        
@@ -67,6 +60,25 @@ public class BusquedasController {
 		
 		model.put("busqueda", busqueda);
 		return new ModelAndView(model, "historico-consultas.hbs");
+	}
+	
+	public static ModelAndView listar (Request req, Response res)
+	{
+		Map<String, List<Busqueda>> model = new HashMap<>();
+		String fechaDesde = req.params("fechaDesde");
+		String fechaHasta = req.params("fechaHasta");
+		//String cantidadPois = req.params("cantidadPois");
+		//String terminal = req.params("terminal");
+		
+		
+		
+		List<Busqueda> busqueda = new ArrayList<Busqueda>();
+		busqueda.addAll((new RepositorioPersistente()).listar(fechaDesde, fechaHasta));
+		//busqueda.addAll(RepositorioNoPersistente.getInstance().listar(fechaDesde,fechaHasta));
+		
+		model.put("busqueda", busqueda);
+		return new ModelAndView(model, "historico-consultas.hbs");
+		
 	}
 	
 	
