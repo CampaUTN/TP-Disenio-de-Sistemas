21a8d6db4880d091c5ac3c75edd377a5f583e909
CampaUTN <rodrigocampassi@hotmail.com>
2016-11-24 04:58:14 -0300

    devuelvo poi controller

---
+++ src/main/java/tpAnual/ui/controllers/PoiController.java
@@ -0,0 +1,57 @@
+package tpAnual.ui.controllers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import tpAnual.Mapa;
+import tpAnual.POIs.Banco;
+import tpAnual.POIs.Negocio;
+import tpAnual.POIs.Poi;
+import tpAnual.util.wrapper.PointWrapper;
+
+public class PoiController {
+
+	public static ModelAndView get(Request req, Response res){
+		
+		Map<String, Object> viewModel = new HashMap<String, Object>();
+
+	 //TODO borrar
+		Poi poi = (Poi)new Negocio(null,"Negocio",null,"compras",5);
+		poi.setCalle("Strangford 1857");
+		
+		viewModel.put("poi", poi);
+		
+		return new ModelAndView(viewModel, "busqueda.hbs");
+	}
+	
+	public static ModelAndView listar(Request req, Response res){
+		
+		//ACA DEBERIAMOS TENER EL REPO QUE SE ENCARGEU DE BUSCAR LOS POIS EN LA BD Y LUEGO MOSTRARLOS		
+		String nombre = req.queryParams("nombre");
+		String tipo = req.queryParams("tipo");
+		
+		Map<String, List<Poi>> model = new HashMap<String, List<Poi>>();
+  		//List<Proyecto> proyectos = RepositorioProyectos.instancia.listar();
+  		List <Poi> resultado = new ArrayList<>();
+		if(nombre.length() == 0 && tipo == "Todos"){
+			System.out.println("ENTRE ACA");
+			resultado = Mapa.getInstance().getPois();
+		}else
+		{
+
+			resultado = Mapa.getInstance().buscarPoi(nombre,tipo);
+		}
+		 
+		
+  		model.put("pois", resultado);
+  		return new ModelAndView(model, "pois.hbs");
+	}
+	
+}
